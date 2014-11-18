cd89e1dccab1215c20de3bd7d354c39d5cc2abcd
Iris Yuan <irisyuan@users.noreply.github.com>
2014-11-17 19:30:25 -0500

    Calculate time it takes video to load on play, pause, etc.

---
+++ loadtime.js
@@ -0,0 +1,31 @@
+/* Console.log time the time it takes video to load on play, pause, etc. */
+/* Uses HTML5 video media events API
+
+(function () {
+
+  var start;
+  var end; 
+  var totalTime;
+
+  window.onload = function() {
+  
+  /* Reset the start time whenever the user seeks a new playback position */
+  document.getElementById('videoplayer').addEventListener("seeking", function() {
+    start = Date.now();
+    console.log("Jumped");
+  }, false);
+  
+  /* When "play" is triggered, the datetimestamp is saved in 'start' */
+  document.getElementById('videoplayer').addEventListener("play", function() {
+    start = Date.now();
+  }, false);
+  
+  /* When video detected as "playing", calculate the difference between 'start' and 'end' */
+  document.getElementById('videoplayer').addEventListener("playing", function() {
+    end = Date.now();
+    totalTime = (end - start) / 1000;
+    console.log(totalTime);
+  }, false);
+  };
+  
+})();
